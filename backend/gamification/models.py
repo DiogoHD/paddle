8417f2488daeb9fe75f36ed1d84c0d1211350a0d
Diogo Neves <nevesdiogo2006@gmail.com>
2026-05-02 04:01:59 +0100

    Adds signals for achievements

--- backend/gamification/models.py
+++ backend/gamification/models.py
@@ -2,9 +2,18 @@ from core.models import BaseModel
 from django.db import models
 
 class Achievement(BaseModel):
+    class RequirementType(models.TextChoices):
+        FRIEND_COUNT = 'friend_count', 'Friend Count'
+        ACCOUNT_AGE = 'account_age', 'Account Age'
+        JOINED_MATCH = 'joined_match', 'Joined Match'
+    
     name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
     description = models.TextField()
     imgSrc = models.URLField(blank=True, null=True)
+    
+    requirement_type = models.CharField(max_length=50, choices=RequirementType.choices)  # e.g., 'friend_count', 'match_wins'
+    requirement_value = models.IntegerField()  # e.g., 10 for 10 friends
 
     def __str__(self):
         return self.name
--- backend/paddle/settings.py
+++ backend/paddle/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     'matches',
     'authentication',
     'friends',
-    'gamification',
+    'gamification.apps.GamificationConfig',
 ]
 
 MIDDLEWARE = [
@@ -155,6 +155,8 @@ SIMPLE_JWT = {
     "BLACKLIST_AFTER_ROTATION": True,
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGGING = {
     'version': 1,
