5473bec27b0757692f7ff97e22c1cf8a6d6cc9ef
Diogo Neves <nevesdiogo2006@gmail.com>
2026-05-02 16:00:40 +0100

    Match Api Calls

--- backend/matches/urls.py
+++ backend/matches/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('<uuid:match_uuid>/', views.get_match_details, name='get-match-details'),
     path('<uuid:match_uuid>/join/', views.join_match, name='join-match'),
     path('<uuid:match_uuid>/leave/', views.leave_match, name='leave-match'),
-    path('user/<uuid:user_uuid>/', views.list_user_matches, name='list-user-matches'),
-    path('user/<uuid:user_uuid>/history/', views.user_match_history, name='user-match-history'),
+    path('me/', views.list_user_matches, name='list-user-matches'),
+    path('me/history/', views.user_match_history, name='user-match-history'),
 ]
